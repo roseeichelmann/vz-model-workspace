5dbb46263be9ac49b3d4b7a468010f485e9afbbb
rose <rose.eichelmann@gmail.com>
2024-03-07 19:03:16 -0600

    sql so far

--- up.sql
+++ up.sql
@@ -1,44 +1,68 @@
+-- ROAD TYPE LOOKUP
+CREATE TABLE public.road_type_lkp (
+    id integer PRIMARY KEY,
+    description text
+);
+
+INSERT INTO public.road_type_lkp (id, description) values (1, 'alley');
+INSERT INTO public.road_type_lkp (id, description) values (2, 'collector');
+INSERT INTO public.road_type_lkp (id, description) values (3, 'arterial');
+INSERT INTO public.road_type_lkp (id, description) values (4, 'highway');
+INSERT INTO public.road_type_lkp (id, description) values (5, 'other');
+
+-- UNIT TYPE LOOKUP
+CREATE TABLE public.unit_type_lkp (
+    id integer PRIMARY KEY,
+    description text
+);
+
+INSERT INTO public.unit_type_lkp (id, description) values (1, 'vehicle');
+INSERT INTO public.unit_type_lkp (id, description) values (2, 'pedestrian');
+INSERT INTO public.unit_type_lkp (id, description) values (3, 'motorcycle');
+INSERT INTO public.unit_type_lkp (id, description) values (4, 'spaceship');
+INSERT INTO public.unit_type_lkp (id, description) values (5, 'bicycle');
+INSERT INTO public.unit_type_lkp (id, description) values (6, 'other');
+
 -- CRASHES
 CREATE TABLE public.cris_crashes (
     crash_id integer PRIMARY KEY,
-    address_confirmed_primary text,
     latitude double precision,
     longitude double precision,
-    road_type_id integer
+    primary_address text,
+    road_type_id integer REFERENCES public.road_type_lkp ON DELETE RESTRICT ON UPDATE RESTRICT
 );
     
 CREATE TABLE public.crashes (
     crash_id integer PRIMARY KEY,
-    address_confirmed_primary text,
     latitude double precision,
     longitude double precision,
-    road_type_id integer,
+    primary_address text,
+    road_type_id integer REFERENCES public.road_type_lkp ON DELETE RESTRICT ON UPDATE RESTRICT,
     vz_location_id text,
-    vz_unique_unit_types integer ARRAY,
+    vz_unique_unit_types integer ARRAY
 );
 
 -- UNITS
 CREATE TABLE public.cris_units (
+    unit_id SERIAL,
     crash_id integer NOT NULL REFERENCES public.cris_crashes ON DELETE CASCADE ON UPDATE CASCADE,
-    unit_nbr integer NOT NULL,
-    unit_desc_id integer,
-    PRIMARY KEY(crash_id, unit_nbr)
+    unit_type_id integer REFERENCES public.unit_type_lkp ON DELETE RESTRICT ON UPDATE RESTRICT,
+    PRIMARY KEY(crash_id, unit_id)
 );
 
 CREATE TABLE public.units (
+    unit_id SERIAL,
     crash_id integer NOT NULL REFERENCES public.crashes ON DELETE CASCADE ON UPDATE CASCADE,
-    unit_nbr integer NOT NULL,
-    unit_desc_id integer,
-    PRIMARY KEY(crash_id, unit_nbr)
+    unit_type_id integer REFERENCES public.unit_type_lkp ON DELETE RESTRICT ON UPDATE RESTRICT,
+    PRIMARY KEY(crash_id, unit_id)
 
 );
 
 -- LOCATIONS
 CREATE TABLE public.locations (
-    location_id character varying NOT NULL,
+    location_id character varying PRIMARY KEY,
     description text NOT NULL,
     address text,
-    metadata json,
     last_update date DEFAULT now() NOT NULL,
     is_retired boolean DEFAULT false NOT NULL,
     is_studylocation boolean DEFAULT false NOT NULL,
@@ -94,18 +118,104 @@ CREATE TABLE public.locations (
     location_group smallint DEFAULT 0
 );
 
--- ROAD TYPE LOOKUP
-CREATE TABLE public.road_type_lkp (
-    road_type_id integer NOT NULL,
-    road_type_desc character varying(128),
-    eff_beg_date character varying(32),
-    eff_end_date character varying(32)
-);
+-- TRIGGERS
 
--- UNIT LOOKUP
-CREATE TABLE public.unit_desc_lkp (
-    unit_desc_id integer NOT NULL,
-    unit_desc_desc character varying(128),
-    eff_beg_date character varying(32),
-    eff_end_date character varying(32)
-);
+-- Trigger on insert to cris table
+
+-- order of these triggers will depend on how cris data is inserted into the db - are crashes first or units?
+CREATE OR REPLACE FUNCTION public.copy_crash_on_cris_insert()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$function$
+DECLARE uniq_unit_types integer ARRAY;
+begin
+    SELECT ARRAY(SELECT DISTINCT units.unit_type_id into uniq_unit_types from units where crash_id = NEW.crash_id);
+  INSERT INTO public.crashes(
+    crash_id,
+    latitude,
+    longitude,
+    primary_address,
+    road_type_id,
+    vz_unique_unit_types
+  )
+  VALUES (
+    NEW.crash_id,
+    NEW.latitude,
+    NEW.longitude,
+    NEW.primary_address,
+    NEW.road_type_id,
+    uniq_unit_types
+  );
+  RETURN NEW;
+end;
+$function$
+;
+
+CREATE OR REPLACE FUNCTION public.copy_unit_on_cris_insert()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$function$
+begin
+  INSERT INTO public.units(
+    unit_id,
+    crash_id,
+    unit_type_id
+  )
+  VALUES (
+    NEW.unit_id,
+    NEW.crash_id,
+    NEW.unit_type_id
+  );
+  RETURN NEW;
+end;
+$function$
+;
+
+CREATE OR REPLACE TRIGGER copy_crash_on_cris_insert
+    AFTER INSERT ON public.cris_crashes FOR EACH ROW EXECUTE FUNCTION public.copy_crash_on_cris_insert();
+
+CREATE OR REPLACE TRIGGER copy_unit_on_cris_insert
+    AFTER INSERT ON public.cris_units FOR EACH ROW EXECUTE FUNCTION public.copy_unit_on_cris_insert();
+
+-- Trigger for location_id
+
+-- This trigger function updates the location_id of a record in atd_txdot_crashes on insert or update.
+CREATE OR REPLACE FUNCTION public.update_crash_location_id()
+    RETURNS TRIGGER AS $$
+    DECLARE crash_position geometry;
+    BEGIN
+        crash_position := ST_SetSRID(ST_MakePoint(NEW.longitude, NEW.latitude), 4326);
+        -- Return the location id of the crash by finding which location polygon the crash
+        -- geographic position resides in
+        NEW.vz_location_id = (
+            SELECT location_id 
+            FROM locations 
+            WHERE (geometry && crash_position)
+            AND ST_Contains(geometry, crash_position)
+            LIMIT 1 --TODO: This should be temporary until we get our polygons in order
+        );
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+-- CREATE OR REPLACE TRIGGER update_crash_location_id
+--     AFTER INSERT OR UPDATE ON public.crashes FOR EACH ROW EXECUTE FUNCTION public.update_crash_location_id();
+
+-- Trigger for vz_unique_unit_types
+
+CREATE OR REPLACE FUNCTION public.update_unique_unit_types()
+    RETURNS TRIGGER AS $$
+    DECLARE uniq_unit_types integer ARRAY;
+    BEGIN
+        SELECT ARRAY(SELECT DISTINCT units.unit_type_id from units where crash_id = NEW.crash_id) into uniq_unit_types;
+        UPDATE public.crashes SET vz_unique_unit_types = uniq_unit_types where crash_id = NEW.crash_id;
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+
+-- need to make sure this runs after the crash has already been created
+CREATE OR REPLACE TRIGGER update_unique_unit_types
+    AFTER INSERT OR UPDATE ON public.units FOR EACH ROW EXECUTE FUNCTION public.update_unique_unit_types();
